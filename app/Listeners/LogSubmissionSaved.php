78eba2ad0415ed3bd04cdfd7b5621d54379ab959
DeyvidA <darauz@nicarao.agency>
2024-02-23 04:48:56 -0600

    Add Submission even saved

---
+++ app/Listeners/LogSubmissionSaved.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SubmissionSaved;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogSubmissionSaved
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(SubmissionSaved $event): void
+    {
+        //
+        Log::info('Submission saved: ' . $event->name . ' - ' . $event->email);
+    }
+}
+
